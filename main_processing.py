dc6c87794c4d8ed9e26e22092d30eee28f2c1849
Anaaim <alexandre.naaim@univ-lyon1.fr>
2026-09-10 11:05:23 +0200

    cleaning correct fq to have smaller function

--- main_processing.py
+++ main_processing.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-from correct_markerless_fq import correct_markerless_fq
+from pre_process import correct_markerless_fq_folder
 
 # defintion of the folder of the data 
 DATA_FOLDER = Path("data/")
@@ -29,10 +29,13 @@ def pre_process_marker_less_data():
     # take into account that it is possible that there is only one marker-less model, but the code should be able to handle multiple models.
     for model in marker_less_model:
         # Pre-process the data for each model
+        # Correction of the frame rate of the marker-less data based on the corresponding marker-based data.
         folder_markerless_to_correct = raw_data_folder / model
         folder_marker_based = raw_data_folder / "marker_based"
-        folder_marker_less_to_export = pp_marker_less_folder /"ML_fq_corrected" / model
-        correct_markerless_fq(folder_marker_based, folder_markerless_to_correct, folder_marker_less_to_export)
+        folder_marker_less_fq_corrected = pp_marker_less_folder /"ML_fq_corrected" / model
+        correct_markerless_fq_folder(folder_marker_based, folder_markerless_to_correct, folder_marker_less_fq_corrected)
+        # trim the marker-less data based on the cut of the marker-based data
+        #trim_marker_less_data(folder_marker_less_fq_corrected,)        
 
 if __name__ == "__main__":
     pre_process_marker_based_data()
